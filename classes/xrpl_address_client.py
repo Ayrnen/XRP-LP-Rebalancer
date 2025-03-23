d70defebcc85f68ae0f03d066bc6162628180dc9
Ayrnen <ayrnen@pm.me>
2025-03-23 16:09:47 -0400

    create class skeleton

---
+++ classes/xrpl_address_client.py
@@ -0,0 +1,21 @@
+from classes.config_reader import ConfigReader
+
+class ETHLAddressClient:
+    def __init__(self, address):
+        self.config = ConfigReader()
+        self.address = address
+        
+    def get_balance_eth(self):
+        pass
+    
+    def get_balance_token(self, token_address):
+        pass
+
+    def get_transaction_count(self):
+        pass
+    
+    def get_lp_position(self, lp_address, lp_abi):
+        pass
+
+    async def get_pending_txns(self):
+        pass
