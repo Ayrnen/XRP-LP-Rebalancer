f4e450d9783bc75f90abcf6d5e84c1787796c6a2
Ayrnen <aymenbelakhoua@gmail.com>
2025-04-09 13:54:16 -0400

    test balance getter methods

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 from classes.config_reader import ConfigReader
 from classes.runtime_tracker import RuntimeTracker
 from classes.rpc_client import RPCClient
-
 from classes.xrpl_address_client import XRPLAddressClient
 
 from dotenv import load_dotenv
@@ -14,9 +13,15 @@ class Placeholder:
 
 
 if __name__ == '__main__':
-    rpc = RPCClient()
-    connected, status_message = rpc.validate_connection_http()
-    print(f"Connected: {connected} | Status: {status_message}")
+    # rpc = RPCClient()
+    # connected, status_message = rpc.validate_connection_http()
+    # print(f"Connected: {connected} | Status: {status_message}")
 
-    connected, status_message = rpc.validate_connection_wss()
-    print(f"Connected: {connected} | Status: {status_message}")
+    # connected, status_message = rpc.validate_connection_wss()
+    # print(f"Connected: {connected} | Status: {status_message}")
+    load_dotenv()
+    address = os.getenv('ADDRESS')
+    address_client = XRPLAddressClient(address)
+ 
+    print(address_client._validate_address())
+    print(address_client.get_balance_xrp())
