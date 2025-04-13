0f1422c17f96cab5b96beac8c9f7ff703c366a60
Ayrnen <aymenbelakhoua@gmail.com>
2025-04-13 15:35:20 -0400

    new class

---
+++ classes/token_client.py
@@ -0,0 +1,14 @@
+class TokenClient:
+    def __init__(self):
+        pass
+
+    def convert_token(self):
+        dict_row = {
+            'type': 'token',
+            'token': 'XRP',
+        }
+        dict_row = {
+            'type': 'LP',
+            'token1': 'XRP',
+            'token2': 'RLUSD',
+        }
