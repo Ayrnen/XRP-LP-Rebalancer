02116e973d24cb4cf527fb51c53e300bcffa2f26
Ayrnen <ayrnen@pm.me>
2025-05-02 12:20:02 -0400

    Add insert statement functions

--- classes/database_manager.py
+++ classes/database_manager.py
@@ -96,3 +96,103 @@ class DatabaseManager:
         """)
         
         conn.commit()
+
+    """
+    Insert statements
+    """
+    def create_snapshot(self, snapshot_type):
+        from datetime import datetime as dt
+        
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO snapshots (timestamp, snapshot_type) VALUES (?, ?)",
+            (dt.now(), snapshot_type)
+        )
+        snapshot_id = cursor.lastrowid
+        conn.commit()
+        return snapshot_id
+    
+    def insert_price_data(self, snapshot_id, token_id, token_symbol, usd_price):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        
+        cursor.execute(
+            """
+            INSERT INTO price_data 
+            (snapshot_id, token_id, token_symbol, usd_price) 
+            VALUES (?, ?, ?, ?)
+            """,
+            (snapshot_id, token_id, token_symbol, usd_price)
+        )
+        conn.commit()
+
+    def insert_amm_data(self, snapshot_id, amm_details):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        
+        cursor.execute(
+            """
+            INSERT INTO amm_data 
+            (snapshot_id, lp_token, lp_issuer, lp_token_amount, 
+            asset1, asset2, asset1_issuer, asset2_issuer, 
+            asset1_amount, asset2_amount, asset1_frozen, asset2_frozen) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """,
+            (
+                snapshot_id, 
+                amm_details['lp_token'], 
+                amm_details['lp_issuer'],
+                amm_details['lp_token_amount'],
+                amm_details['asset1'],
+                amm_details['asset2'],
+                amm_details.get('asset1_issuer'),
+                amm_details['asset2_issuer'],
+                amm_details['asset1_amount'],
+                amm_details['asset2_amount'],
+                amm_details.get('asset1_frozen', False),
+                amm_details.get('asset2_frozen', False)
+            )
+        )
+        conn.commit()
+
+    def insert_balance_data(self, snapshot_id, address, token_type, token_issuer, balance, usd_value=None):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        
+        cursor.execute(
+            """
+            INSERT INTO balance_data 
+            (snapshot_id, address, token_type, token_issuer, balance, usd_value) 
+            VALUES (?, ?, ?, ?, ?, ?)
+            """,
+            (snapshot_id, address, token_type, token_issuer, balance, usd_value)
+        )
+        conn.commit()
+
+    def insert_lp_position_data(self, snapshot_id, address, lp_token, lp_issuer, lp_token_amount, 
+                            ownership_percentage, asset1_amount, asset2_amount, total_usd_value=None):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        
+        cursor.execute(
+            """
+            INSERT INTO lp_position_data 
+            (snapshot_id, address, lp_token, lp_issuer, lp_token_amount, 
+            ownership_percentage, asset1_amount, asset2_amount, total_usd_value) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """,
+            (
+                snapshot_id,
+                address,
+                lp_token,
+                lp_issuer,
+                lp_token_amount,
+                ownership_percentage,
+                asset1_amount,
+                asset2_amount,
+                total_usd_value
+            )
+        )
+        conn.commit()
+
