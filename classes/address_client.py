658a372969277e79bd33f17043438a4fc84e3385
Ayrnen <ayrnen@pm.me>
2025-05-10 02:58:27 -0400

    Small syntax fixes

--- classes/address_client.py
+++ classes/address_client.py
@@ -2,7 +2,6 @@ from classes.config_reader import ConfigReader
 from classes.rpc_client import RPCClient
 from classes.amm_client import AMMClient
 from xrpl.core import addresscodec
-from xrpl.core.addresscodec import XRPLAddressCodecException
 
 class AddressClient:
     def __init__(self, address):
@@ -27,20 +26,19 @@ class AddressClient:
         raw_data = self.rpc.get_token_trust_line(self.address, token_address)
         parsed_data = self._parse_trust_line_info(raw_data)
         return parsed_data
+    
     def _parse_trust_line_info(self, raw_data):
         raise NotImplementedError('Parsing trust line information not implemented')
     
     def get_balance_token(self, token_address):
         raw_data = self.rpc.get_token_trust_line(self.address, token_address)
         parsed_data = self._parse_token_balance(raw_data)
-        return parsed_data, None
+        return parsed_data
+    
     def _parse_token_balance(self, raw_data):
-        # Check if there are any lines (user has a trust line for this token)
         if 'result' in raw_data and 'lines' in raw_data['result'] and len(raw_data['result']['lines']) > 0:
             return raw_data['result']['lines'][0]['balance']
-        # If no lines, return 0 (user has no trust line for this token)
         return 0
-        
 
     def get_transaction_count(self):
         pass
@@ -49,18 +47,16 @@ class AddressClient:
         raw_data = self.rpc.get_amm_position(self.address, lp_issuer, lp_token)
         parsed_data = self._parse_lp_balance(raw_data)
         return float(parsed_data)
+    
     def _parse_lp_balance(self, raw_data):
-        # Check if there are any lines (user has LP tokens)
         if 'result' in raw_data and 'lines' in raw_data['result'] and len(raw_data['result']['lines']) > 0:
             return raw_data['result']['lines'][0]['balance']
-        # If no lines, return 0 (user has no LP tokens)
         return 0
 
     def get_lp_breakdown(self, lp_token, lp_issuer, token1, token2):
         lp_token_amount = self.get_lp_balance(lp_issuer, lp_token)
         breakdown_data = self.amm.get_position_breakdown(lp_issuer, token1, token2, lp_token_amount)
         return breakdown_data
-
 
     async def get_pending_txns(self):
         pass
