c33e3d8466c2cbf08e6f06ee92c35e99f5e6cae8
Ayrnen <ayrnen@pm.me>
2025-05-10 14:55:45 -0400

    Access issue fix

--- run_data_collector.sh
+++ run_data_collector.sh
@@ -4,10 +4,10 @@
 cd "$HOME/Projects/XRP-LP-Rebalancer"
 
 # Activate the xrp virtual environment
-source xrp/bin/activate
+source "$HOME/Projects/XRP-LP-Rebalancer/xrp/bin/activate"
 
-# Run the data collector script
-python data_collector.py
+# Run the data collector script with full path to Python
+"$HOME/Projects/XRP-LP-Rebalancer/xrp/bin/python" data_collector.py
 
 # Deactivate virtual environment
 deactivate
